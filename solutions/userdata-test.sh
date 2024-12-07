23946e40f882660548a1c90b2d9d5dc0cc1b1b5f
aredo <chris119odu@gmail.com>
2024-12-07 01:59:33 +0100

    added

--- solutions/userdata-test.sh
+++ solutions/userdata-test.sh
@@ -6,7 +6,7 @@ pip3 install Flask==2.3.3
 pip3 install Flask-MySql
 pip3 install boto3
 dnf install git -y
-echo "osvaldo-phonebook-18.cbanmzptkrzf.us-east-1.rds.amazonaws.com" > /home/ec2-user/dbserver.endpoint
+echo "${MyDBURI}"  > /home/ec2-user/dbserver.endpoint
 cd /home/ec2-user
 TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /clarusway/phonebook/token --with-decryption --query 'Parameter.Value' --output text)
 git clone https://$TOKEN@github.com/aredo01/phone-web-app.git
